df60c90cef61b4cd8e7d3417f5d822392259471e
denis <warning11223@gmail.com>
2021-04-27 15:32:21 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    $('.carousel__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2,
+                },
+
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                },
+
+            }
+        ],
+
+    })
+
+});
